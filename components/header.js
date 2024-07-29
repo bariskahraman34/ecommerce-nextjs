01e3dfb4bd8ff65e3a978618f1b3d1a1e05ef6be
Barış Kahraman <baris-kahra@hotmail.com>
2024-07-29 17:32:37 +0300

    fetch dummyjson products and sort top 4 categories to show in header.

--- components/header.js
+++ components/header.js
@@ -1,7 +1,29 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export default function Header(){
+export default async function Header(){
+
+  const getData = await fetch("https://dummyjson.com/products?limit=100");
+  const response = await getData.json();
+  const products = response.products;
+
+  const categoryCount = products.reduce((acc, product) => {
+    acc[product.category] = (acc[product.category] || 0) + 1;
+    return acc;
+  }, {});
+
+  const categories = Object.entries(categoryCount);
+  
+  const sortedCategories = categories.sort((a,b) => a[1]-b[1])
+
+  function categoryRegEx(text) {
+    return text.replace(/(?:^|\s)\S+/g, function(match) {
+        return match.charAt(0).toUpperCase() + match.slice(1).toLowerCase();
+    }).replace(/[-_](\w)/g, function(match, p1) {
+        return " " + p1.toUpperCase();
+    });
+  }
+  
   return (
     <header className="top-header">
       <div className="header-left-side">
@@ -10,18 +32,16 @@ export default function Header(){
         </Link>
         <nav className="nav">
           <ul className="nav-elements-container">
-            <li><a href="#" className="nav-element">Koleksiyon</a></li>
-            <li><a href="#" className="nav-element">Erkek</a></li>
-            <li><a href="#" className="nav-element">Kadın</a></li>
-            <li><a href="#" className="nav-element">Hakkında</a></li>
-            <li><a href="#" className="nav-element">İletişim</a></li>
-            <li><a href="user-product-page.html" className="nav-element">Kullanıcı Sayfası</a></li>
+            <li><Link href="/products" className="nav-element">Products</Link></li>
+            {sortedCategories.reverse().slice(0,4).map((category,index) => (
+              <li key={index}><Link href={category[0]} className="nav-element">{categoryRegEx(category[0])}</Link></li>
+            ))}
           </ul>
         </nav>
       </div>
       <div className="header-right-side">
         <button className="basket-btn">
-          <span className="basket-quantity"></span>
+          <span className="basket-quantity">0</span>
           <Image src="/icons/basket.svg" width="20" height="20" alt="basket" className="basket" />
         </button>
         <div className="basket-result">
