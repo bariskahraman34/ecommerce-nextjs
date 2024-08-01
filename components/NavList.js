943fd03965e04323661e6aad77d8b6003f3b2bae
Barış Kahraman <baris-kahra@hotmail.com>
2024-08-02 00:00:02 +0300

    feat: Add basket

--- components/NavList.js
+++ components/NavList.js
@@ -3,15 +3,18 @@ import Link from "next/link";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { usePathname, useSearchParams } from 'next/navigation'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavList({products}){
-  const [link, setLink] = useState(null);
+  const [link, setLink] = useState("");
+  const [isBasketOpen , setIsBasketOpen] = useState(false);
   const pathname = usePathname()
   const searchParams = useSearchParams()
 
   useEffect(() => {
-    setLink(`${pathname}?${searchParams}`)
-  },[pathname, searchParams])
+    setLink(searchParams.get('category'))
+  },[searchParams])
 
   const categoryCount = products.reduce((acc, product) => {
     acc[product.category] = (acc[product.category] || 0) + 1;
@@ -29,7 +32,11 @@ export default function NavList({products}){
         return " " + p1.toUpperCase();
     });
   }
-  
+
+  function toggleBasket(){
+    setIsBasketOpen(() => !isBasketOpen)
+  }
+
   return (
     <header className="top-header">
       <div className="header-left-side">
@@ -38,21 +45,46 @@ export default function NavList({products}){
         </Link>
         <nav className="nav">
           <ul className="nav-elements-container">
-            <li><Link href="/products" className={`nav-element ${link == "/products?" && "active"}`}>Products</Link></li>
+            <li><Link href="/products" className={`nav-element ${(!link && pathname == "/products") ? "active" : ""}`}>Products</Link></li>
             {sortedCategories.reverse().slice(0,4).map((category,index) => (
-              <li key={index}><Link href={`${pathname}?category=${category[0]}`} className={`nav-element ${`${pathname}?category=${category[0]}` == `${link}` && "active"}`}>{categoryRegEx(category[0])}</Link></li>
+              <li key={index}><Link href={`/products/?category=${category[0]}`} className={`nav-element ${(searchParams.get('category') && category[0] == link) ? "active" : ""}`}>{categoryRegEx(category[0])}</Link></li>
             ))}
           </ul>
         </nav>
       </div>
       <div className="header-right-side">
-        <button className="basket-btn">
+        <button className="basket-btn" onClick={toggleBasket}>
           <span className="basket-quantity">0</span>
           <Image src="/icons/basket.svg" width="20" height="20" alt="basket" className="basket" />
         </button>
-        <div className="basket-result">
-
+        <div className={`basket-result ${isBasketOpen && "show"}`}>
+          {isBasketOpen && (
+            <>
+            <div className="cart-header">
+              <button className="close-offcanvas-btn" onClick={toggleBasket}><FontAwesomeIcon size="xl" icon={faXmark} /></button>
+              <h3>Cart</h3>
+            </div>
+            <div className="cart-body">
+              <div className="cart-container">            
+                  <Image src="/icons/del-icon.svg" width={50} height={50} alt="" />
+                  <div className="cart-infos">
+                      <div>
+                          <span className="thin-text">basket.title</span>
+                      </div>
+                      <div> 
+                          <span className="product-price thin-text">$basket.productPrice</span>
+                          <span className="thin-text">x</span>
+                          <span className="thin-text">basket.quantity</span>
+                          <span className="bold-text">$basket.totalPrice</span>
+                      </div>
+                  </div>
+              </div>
+              <button className="delete-btn" ><Image className="del-icon" src="/icons/del-icon.svg" width={15} height={15} alt=""/></button>
+            </div>
+            </>
+          )}
         </div>
+
         <button>
             <Image src="/img/user-image.png" width="40" height="40" alt="user image" className="user-profile-img" />
         </button>
